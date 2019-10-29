41c65d885a9134fa720301a91e305ba40ca3127e
Emil Hein Eriksen <emilhein1@gmail.com>
2019-10-29 14:50:39 +0100

    404 text

---
+++ src/views/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This page does not exist
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "notFound",
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
