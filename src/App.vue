7bd647f97961c8a277c49258215b1869e8d7290f
Emilhein <emilhein1@gmail.com>
2024-01-26 12:11:26 +0100

    Add spinner component

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,22 @@ const tabStore = useTabStore()
 </script>
 
 <template>
-  <header>
-    <el-tabs v-model="tabStore.tab"  tab-position="top"  class="demo-tabs" >
-      <el-tab-pane label="Design 1" name="MediumPreview" />
-      <el-tab-pane label="Design 2" name="MediumPreview2" />
-    </el-tabs>
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
+  <el-container>
 
-      <!-- <nav> -->
-      <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
-      <!-- </nav> -->
-    </div>
-  </header>
 
-  <RouterView />
+    <el-header>
+      <el-tabs v-model="tabStore.tab" tab-position="top" class="demo-tabs">
+        <el-tab-pane label="Design 1" name="MediumPreview" />
+        <el-tab-pane label="Design 2" name="MediumPreview2" />
+      </el-tabs>
+
+
+    </el-header>
+    <el-main>
+      <RouterView />
+
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>
@@ -34,65 +34,4 @@ const tabStore = useTabStore()
   font-weight: 600;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/components/postList.vue
+++ src/components/postList.vue
@@ -1,37 +1,54 @@
 <script setup>
 import { usePostsStore } from './../stores/posts'
 import { useTabStore } from './../stores/tab'
+import Spinner from './spinner.vue'
+import { computed } from 'vue'
+
 import MediumPreview from './blogDesigns/MediumPreview.vue'
 import MediumPreview2 from './blogDesigns/MediumPreview2.vue'
 
 const componentMap = { MediumPreview, MediumPreview2 }
 const tabStore = useTabStore()
 const postStore = usePostsStore()
+const loading = computed(() => {
+  return postStore.posts.length === 0
+})
 </script>
 
 <template>
   <div class="container">
-    <KeepAlive>
-
-    <component :is="componentMap[tabStore.tab]" :post="post" v-for="(post) in postStore.posts" :key="post.title">
-    </component>
-  </KeepAlive>
-
+    <!-- <Spinner v-if="loading"></Spinner> -->
+    <KeepAlive >
+      <Transition  name="translate" mode="out-in" v-for="(post) in postStore.posts" :key="post.title">
+        <component :is="componentMap[tabStore.tab]" :post="post" />
+      </Transition>
+    </KeepAlive>
   </div>
 </template>
 
 <style scoped>
-/* we will explain what these classes do next! */
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
+.translate-enter-active {
+  animation: bounce-in 0.5s;
 }
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
+.translate-leave-active {
+  animation: bounce-in 0.5s reverse;
 }
 
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    transform: scale(1.25);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
 
 .container {
   display: flex;
